62b8b2d6fed79def7e147f1648142d3985d4a6de
lowright09 <lowright09@gmail.com>
2023-05-04 12:27:42 -0500

    first push

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,7 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
+        System.out.println("hello");
         System.out.println("printJobs is not implemented yet");
     }
 }
